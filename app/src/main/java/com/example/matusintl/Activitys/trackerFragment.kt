754a8c04e27291da5ee4e871484a3c173a7e024d
kathorecinos <76710595+kathorecinos@users.noreply.github.com>
2021-06-08 15:59:56 -0500

    dd

--- app/src/main/java/com/example/matusintl/Activitys/trackerFragment.kt
+++ app/src/main/java/com/example/matusintl/Activitys/trackerFragment.kt
@@ -66,7 +66,7 @@ class trackerFragment : Fragment() {
                 dato.toString().equals("search")
 
             } else {
-                Log.d
+
             }
 
             activity?.let {
